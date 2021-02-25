8d235b2c41d55c8744566570c4915a5f5180f93b
明洁 <3284304056@qq.com>
2021-02-25 16:30:31 +0800

    feat: 歌单item完成

--- src/pages/main/playList/pages/perPlayList/components/PlayListItem/index.js
+++ src/pages/main/playList/pages/perPlayList/components/PlayListItem/index.js
@@ -1,30 +1,86 @@
 import React from 'react';
 import {View, Text, StyleSheet, ImageBackground} from 'react-native';
-export const PlayListItem = (props) => {
+import {connect} from 'react-redux';
+import IconFont from '../../../../../../../iconfont';
+const _PlayListItem = (props) => {
   const {info} = props;
   const source = {uri: info.coverImgUrl};
+  const name = info.creator.nickname;
+  const playCount = info.playCount;
+  const description = info.name;
   return (
     <View style={styles.playListItem}>
       <ImageBackground source={source} style={styles.coverImg}>
-        <Text>111</Text>
+        <View style={[styles.itemContainer, styles.topItemContainer]}>
+          <IconFont name="biaoqiantubiao22" size={25} color="#fff" />
+          <Text style={styles.text1}>{playCount}</Text>
+        </View>
+        <View style={styles.itemContainer}>
+          <IconFont name="denglu" size={25} color="#fff" />
+          <Text
+            style={[styles.text1, styles.textName]}
+            numberOfLines={1}
+            ellipsizeMode={'tail'}>
+            {name}
+          </Text>
+        </View>
       </ImageBackground>
+      <View style={styles.playListItemDescription}>
+        <Text style={styles.text2} numberOfLines={2} ellipsizeMode={'tail'}>
+          {description}
+        </Text>
+      </View>
     </View>
   );
 };
+const stateMapToProps = (state) => {
+  return {
+    theme: state.theme,
+  };
+};
+export const PlayListItem = connect(stateMapToProps)(_PlayListItem);
 const styles = StyleSheet.create({
   playListItem: {
     width: 170,
-    height: 170,
+    height: 190,
     borderRadius: 10,
     justifyContent: 'center',
     alignItems: 'center',
   },
   coverImg: {
     width: 170,
-    flex: 1,
+    flex: 170,
     resizeMode: 'cover',
-    justifyContent: 'flex-end',
     borderRadius: 10,
     overflow: 'hidden',
+    justifyContent: 'space-between',
+  },
+  topItemContainer: {
+    justifyContent: 'flex-end',
+  },
+  itemContainer: {
+    width: 170,
+    justifyContent: 'flex-start',
+    padding: 12,
+    flexDirection: 'row',
+    alignItems: 'center',
+  },
+  playListItemDescription: {
+    height: 15,
+    width: 170,
+    marginTop: 5,
+  },
+  text1: {
+    fontSize: 12,
+    color: '#fff',
+    marginLeft: 5,
+  },
+  text2: {
+    fontSize: 12,
+    color: '#333',
+    width: 170,
+  },
+  textName: {
+    width: 80,
   },
 });
