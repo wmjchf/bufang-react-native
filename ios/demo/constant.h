566eb08cccbb68371f018ea3f88983b79d8d0321
wangmingjie <15997129014@163.com>
2021-04-11 16:24:26 +0800

    feat: ios发送短信验证码

---
+++ ios/demo/constant.h
@@ -0,0 +1,15 @@
+//
+//  constant.h
+//  demo
+//
+//  Created by wangmingjie on 2021/4/11.
+//
+
+#ifndef constant_h
+#define constant_h
+
+
+#endif /* constant_h */
+#define UM_AppKey @"6072782018b72d2d244e022d"
+#define UM_ChannelId @"App Store"
+#define UM_AppScrete @"ohc/UvyXQbdcpTTn+M5x/AlS1KNmkeMeNo6+mu3kVW0="
